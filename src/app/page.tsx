c64a0ed46b360e2c8a4851f615c106c635640f83
quetrea <quetrea@hotmail.com>
2025-06-14 22:38:53 +0300

    05: auth stages - feat: implement authentication views and social login options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,18 @@
-"use client";
-import { useState } from "react";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { authClient } from "@/lib/auth-client";
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
-export default function Home() {
-  const { data: session } = authClient.useSession();
-
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit = () => {
-    authClient.signUp.email(
-      {
-        email,
-        name,
-        password,
-      },
-      {
-        onRequest: () => {
-          //show loading
-        },
-        onSuccess: () => {
-          //redirect to the dashboard or sign in page
-          alert("Succesfully created");
-        },
-        onError: (ctx) => {
-          // display the error message
-          alert(ctx.error.message);
-        },
-      }
-    );
-  };
-
-  const onLogin = () => {
-    authClient.signIn.email(
-      {
-        email,
-        password,
-      },
-      {
-        onRequest: () => {
-          //show loading
-        },
-        onSuccess: () => {
-          //redirect to the dashboard or sign in page
-          alert("Succesfully joined");
-        },
-        onError: (ctx) => {
-          // display the error message
-          alert(ctx.error.message);
-        },
-      }
-    );
-  };
-
-  if (session?.session) {
-    return (
-      <div className="flex flex-col p-4 gap-y-4">
-        <p>Logged in as {session.user.name}</p>
-        <Button onClick={() => authClient.signOut()}>Sign out</Button>
-      </div>
-    );
+  if (!session) {
+    redirect("/sign-in");
   }
 
-  return (
-    <div className="flex flex-col gap-y-10">
-      <div className="p-6 flex flex-col gap-y-4">
-        <Input
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+  return <HomeView />;
+};
 
-        <Input
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onSubmit}>Create user</Button>
-      </div>
-
-      <div className="flex flex-col gap-y-4 p-4">
-        <Input
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onLogin}>Login user</Button>
-      </div>
-    </div>
-  );
-}
+export default Page;
