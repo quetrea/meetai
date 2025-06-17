043d0d19b39baa580714b2581b348cf36bd16db7
quetrea <quetrea@hotmail.com>
2025-06-17 23:18:43 +0300

    09: agents setup

---
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/loading-state";
+
+export const AgentsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="Please wait while we load the agents."
+    />
+  );
+};
+
+export const AgentsViewError = () => {
+  return (
+    <LoadingState
+      title="Error Loading Agents"
+      description="There was an error loading the agents. Please try again later."
+    />
+  );
+};
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "world" }));
-  return <div>{data?.greeting}</div>;
+  return <div>Home </div>;
 };
